879b66ea1e60668e7fbf6a431066e7e5d3f3ef6a
Jason Kulatunga <jason@thesparktree.com>
2023-10-12 17:04:58 -0700

    fixing bluebutton patient access url.

--- definitions/internal/platform/bluebutton.go
+++ definitions/internal/platform/bluebutton.go
@@ -42,7 +42,7 @@ func GetSourceBluebutton(env pkg.FastenLighthouseEnvType, clientIdLookup map[pkg
 	sourceDef.SourceType = pkg.SourceTypeBluebutton
 	sourceDef.Category = []string{"Hospital"}
 	sourceDef.Aliases = []string{}
-	sourceDef.PatientAccessUrl = "https://sandbox.bluebutton.cms.gov/"
+	sourceDef.PatientAccessUrl = "https://www.medicare.gov"
 
 	return sourceDef, nil
 }
