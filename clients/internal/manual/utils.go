309b71040fe170037deda28236522543d34d4523
Jason Kulatunga <jason@thesparktree.com>
2023-11-08 14:51:14 -0800

    make sure that Fasten-sources can detect and parse various FHIR record types.

--- clients/internal/manual/utils.go
+++ clients/internal/manual/utils.go
@@ -1,17 +1,31 @@
 package manual
 
 import (
+	"encoding/json"
+	"fmt"
+	fhir401utils "github.com/fastenhealth/gofhir-models/fhir401/utils"
+	"io"
 	"net/http"
 	"os"
 	"strings"
 )
 
+type DocumentType string
+
+const (
+	DocumentTypeCCDA       DocumentType = "CCDA"
+	DocumentTypeFhirBundle DocumentType = "FHIR_BUNDLE"
+	DocumentTypeFhirNDJSON DocumentType = "NDJSON"
+)
+
 /*
-GetFileContentType returns the content type of given file by sniffing out fist 512 bytes of the file
-We can't depend on just http.DetectContentType because it doesn't detect the difference between JSON and XML.
-Instead we'll try to determine the content type ourselves.
+GetFileDocumentType returns the document type of given file.
+We'll use various techniques to determine if the file is XML or JSON-like first.
+If the file is JSON, we'll determine if it's a FHIR bundle.
+If the file is NDJson, we'll parse the first entry to ensure it's a valid fhir resource.
+We can't depend on just http.DetectContentType because it doesn't detect the difference between JSON, NDJSON, FHIR, XML and CCDAs.
 */
-func GetFileContentType(file *os.File) (string, error) {
+func GetFileDocumentType(file *os.File) (DocumentType, error) {
 
 	// Only the first 512 bytes are used to sniff the content type.
 	buffer := make([]byte, 512)
@@ -19,6 +33,7 @@ func GetFileContentType(file *os.File) (string, error) {
 	if err != nil {
 		return "", err
 	}
+	defer file.Seek(0, io.SeekStart)
 
 	contentType := http.DetectContentType(buffer)
 	contentTypeParts := strings.Split(contentType, ";")
@@ -31,39 +46,50 @@ func GetFileContentType(file *os.File) (string, error) {
 
 		//determine the content-type using buffer
 		if firstNonWS == len(buffer) {
-			return "text/plain", nil
+			return DocumentType(contentTypeParts[0]), fmt.Errorf("file is empty")
 		} else if buffer[firstNonWS] == '<' {
-			return "application/xml", nil
-		} else if buffer[firstNonWS] == '{' || buffer[firstNonWS] == '[' {
-			return "application/json", nil
+			//TODO: we should do some additional parsing to confirm
+			return DocumentTypeCCDA, nil
+		} else if buffer[firstNonWS] == '{' {
+			//reset file to beginning
+			file.Seek(0, io.SeekStart)
+
+			//this is 1 or more json documents
+			documentCount := 0
+			containsFhirResource := false
+			d := json.NewDecoder(file)
+			for {
+
+				var resource json.RawMessage
+				err := d.Decode(&resource)
+				if err != nil {
+					// io.EOF is expected at end of stream.
+					if err == io.EOF {
+						break //we're done
+					} else {
+						continue //skip this document, don't add it to the documentCount
+					}
+				}
+
+				//we have a valid json document, lets check if it's a FHIR resource
+				if !containsFhirResource {
+					_, err := fhir401utils.MapToResource(resource, false)
+					if err == nil {
+						containsFhirResource = true
+					}
+				}
+
+				documentCount++
+			}
+
+			if containsFhirResource && documentCount > 1 {
+				return DocumentTypeFhirNDJSON, nil
+			} else if containsFhirResource && documentCount == 1 {
+				return DocumentTypeFhirBundle, nil
+			}
 		}
-		return "text/plain", nil
 	}
-	return contentTypeParts[0], nil
-
-	//if contentTypeParts[0] == "text/plain" {
-	//	// If the content type is text/plain, we'll try to determine the content type ourselves.
-	//	// We'll check if the first non-whitespace character is '<' or '{' or '['.
-	//	// If it is '<', we'll assume it's XML.
-	//	// If it is '{' or '[', we'll assume it's JSON.
-	//	// Otherwise, we'll assume it's text/plain.
-	//	firstNonWS := 0
-	//	for ; firstNonWS < len(buffer) && isWhitespaceChar(buffer[firstNonWS]); firstNonWS++ {
-	//		if firstNonWS == len(buffer) {
-	//			// The file is empty.
-	//			return "text/plain", nil
-	//		} else if buffer[firstNonWS] == '<' {
-	//			return "application/xml", nil
-	//		} else if buffer[firstNonWS] == '{' || buffer[firstNonWS] == '[' {
-	//			return "application/json", nil
-	//		} else {
-	//			return "text/plain", nil
-	//		}
-	//
-	//	}
-	//}
-
-	//return contentType, nil
+	return DocumentType(contentTypeParts[0]), fmt.Errorf("unknown document type, content-type: %s", contentTypeParts[0])
 }
 
 // isWhitespaceChar reports whether the provided byte is a whitespace byte (0xWS)
--- clients/internal/manual/utils_test.go
+++ clients/internal/manual/utils_test.go
@@ -13,9 +13,38 @@ func TestGetFileContentType_Xml(t *testing.T) {
 	require.NoError(t, err)
 
 	//test
-	contentType, err := GetFileContentType(file)
+	contentType, err := GetFileDocumentType(file)
 
 	//assert
 	require.NoError(t, err)
-	require.Equal(t, contentType, "application/xml")
+	require.Equal(t, contentType, DocumentTypeCCDA)
+}
+
+func TestGetFileDocumentType(t *testing.T) {
+
+	var documentTypeTests = []struct {
+		fixturePath          string       // input
+		expectedSuccess      bool         // expected result
+		expectedDocumentType DocumentType // expected result
+	}{
+		{"testdata/fixtures/401-R4/bundle/synthea_Tania553_Harris789_545c2380-b77f-4919-ab5d-0f615f877250.json", true, DocumentTypeFhirBundle},
+		{"testdata/fixtures/401-R4/ccda/MaryGrant-ClinicalSummary.xml", true, DocumentTypeCCDA},
+		{"testdata/fixtures/401-R4/international-patient-summary/IPS-bundle-01.json", true, DocumentTypeFhirBundle},
+		{"testdata/fixtures/401-R4/phr-ndjson-jsonl/JohnDoe.phr", true, DocumentTypeFhirNDJSON},
+		{"testdata/fixtures/401-R4/phr-ndjson-jsonl/TimmySmart-FosterCareTimeline.phr", true, DocumentTypeFhirNDJSON},
+	}
+
+	for _, tt := range documentTypeTests {
+		file, err := os.OpenFile(tt.fixturePath, os.O_RDONLY, 0644)
+		require.NoError(t, err)
+
+		//test
+		contentType, err := GetFileDocumentType(file)
+		if tt.expectedSuccess {
+			require.NoError(t, err)
+			require.Equal(t, contentType, tt.expectedDocumentType, "fixture: %s", tt.fixturePath)
+		} else {
+			require.Error(t, err, "fixture: %s", tt.fixturePath)
+		}
+	}
 }
