d1ae42d9dd7dc198efb42a5d2b5137296ac2255b
Jason Kulatunga <jason@thesparktree.com>
2024-06-05 08:00:05 -0700

    fixing uuid typescript error.

--- js/src/utils/uuid.ts
+++ js/src/utils/uuid.ts
@@ -1,6 +1,7 @@
 export function uuidV4(): string {
     // https://stackoverflow.com/questions/55853005/error-ts2365-operator-cannot-be-applied-to-types-number-and-1000
     return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+        // @ts-ignore
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
 }
