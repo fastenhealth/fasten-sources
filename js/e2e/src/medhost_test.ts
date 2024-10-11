9f8f974e4f74445e8b9eccbc6dca3fdc1ff1dc50
Osama Imtiaz <49908243+sizets@users.noreply.github.com>
2024-10-10 22:30:46 -0400

    tests fixed

--- js/e2e/src/medhost_test.ts
+++ js/e2e/src/medhost_test.ts
@@ -1,11 +1,11 @@
 import { test, expect } from "@playwright/test";
-import {getEndpointDefinition} from '../utils';
-import {generateSourceAuthorizeUrl} from '../../src/connect/authorization-url';
+import { getEndpointDefinition } from '../utils';
+import { generateSourceAuthorizeUrl } from '../../src/connect/authorization-url';
 import process from 'process';
 
-test("MedHost Login Flow", async ({page}, testInfo) => {
+test("MedHost Login Flow", async ({ page }, testInfo) => {
     try {
-        await page.evaluate(_ => {},`browserstack_executor: ${JSON.stringify({action: "setSessionName", arguments: {name:testInfo.title}})}`);
+        await page.evaluate(_ => { }, `browserstack_executor: ${JSON.stringify({ action: "setSessionName", arguments: { name: testInfo.title } })}`);
         await page.waitForTimeout(5000);
         //get the Medhost Sandbox endpoint definition
         let endpointDefinition = await getEndpointDefinition('9e5d5b7a-880f-481b-8ae4-77a3d24cfa49')
@@ -25,11 +25,15 @@ test("MedHost Login Flow", async ({page}, testInfo) => {
         await page.keyboard.type(process.env.PW_MEDHOST_PASSWORD);
         await page.click('#signin');
 
+        await page.waitForSelector("text=Know who you are");
+        await page.check('#acceptedScopes1');
+        await page.click('#submit');
+
         // If successful, Fasten Lighthouse page should now be visible
         await page.waitForSelector("text=Your account has been securely connected to FASTEN.");
-        await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {status: 'passed',reason: 'Authentication Successful'}})}`);
+        await page.evaluate(_ => { }, `browserstack_executor: ${JSON.stringify({ action: 'setSessionStatus', arguments: { status: 'passed', reason: 'Authentication Successful' } })}`);
     } catch (e) {
         console.log(e);
-        await page.evaluate(_ => {}, `browserstack_executor: ${JSON.stringify({action: 'setSessionStatus',arguments: {status: 'failed',reason: 'Test failed'}})}`);
+        await page.evaluate(_ => { }, `browserstack_executor: ${JSON.stringify({ action: 'setSessionStatus', arguments: { status: 'failed', reason: 'Test failed' } })}`);
     }
 });
