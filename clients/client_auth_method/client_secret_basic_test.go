c083abc2707cacb2bedc17ff2d2613611664263c
Jason Kulatunga <jason@thesparktree.com>
2025-12-25 19:14:10 -0500

    adding basic token introspect.

--- clients/client_auth_method/client_secret_basic_test.go
+++ clients/client_auth_method/client_secret_basic_test.go
@@ -3,7 +3,10 @@ package client_auth_method_test
 import (
 	"context"
 	"github.com/fastenhealth/fasten-sources/clients/client_auth_method"
+	"github.com/fastenhealth/fasten-sources/clients/models"
 	"github.com/fastenhealth/fasten-sources/clients/testutils"
+	definitionsModels "github.com/fastenhealth/fasten-sources/definitions/models"
+	"github.com/fastenhealth/fasten-sources/pkg/models/catalog"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -39,4 +42,37 @@ func TestClientBasicSecretTokenRefreshToken_Epic(t *testing.T) {
 	assert.Equal(t, "PLACEHOLDER_NEW_ACCESS_TOKEN", response.AccessToken)
 	assert.Equal(t, "PLACEHOLDER_NEW_REFRESH_TOKEN", response.RefreshToken)
 	assert.Equal(t, int64(3599), response.ExpiresIn)
+}
+
+// Flatiron tests
+func TestClientBasicSecretTokenIntrospectToken_Flatiron(t *testing.T) {
+	//setup
+	endpointDef := definitionsModels.LighthouseSourceDefinition{
+		PatientAccessEndpoint: &catalog.PatientAccessEndpoint{
+			IntrospectionEndpoint: "https://accounts.flatiron.com/connect/introspect",
+		},
+	}
+
+	testHttpClient := testutils.OAuthVcrSetup(t, false)
+
+	//test
+	response, err := client_auth_method.ClientSecretBasicAuthIntrospectToken(
+		context.TODO(),
+		logrus.New(),
+		&oauth2.Config{
+			ClientID:     "PLACEHOLDER_CLIENT_ID",
+			ClientSecret: "PLACEHOLDER_CLIENT_SECRET",
+		},
+		endpointDef,
+		models.TokenIntrospectTokenTypeRefresh,
+		"PLACEHOLDER_REFRESH_TOKEN",
+		testHttpClient,
+	)
+	require.NoError(t, err)
+
+	//assert
+	assert.NotNil(t, response)
+	assert.True(t, response.Active)
+	assert.Equal(t, "fhirUser openid offline_access patient/AllergyIntolerance.read patient/CarePlan.read patient/CareTeam.read patient/Condition.read patient/Device.read patient/DiagnosticReport.read patient/DocumentReference.read patient/Encounter.read patient/Goal.read patient/Immunization.read patient/Observation.read patient/Location.read patient/Medication.read patient/MedicationRequest.read patient/Organization.read patient/Patient.read patient/Practitioner.read patient/Procedure.read patient/Provenance.read", response.Scope)
+	assert.Equal(t, "PLACEHOLDER_CLIENT_ID", response.ClientID)
 }
