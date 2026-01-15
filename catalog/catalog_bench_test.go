e7ed34f7ddc9dd1c1faffe040c1571b22de829fe
Jason Kulatunga <jason@thesparktree.com>
2026-01-14 23:23:44 -0500

    adding result count to benchmarks. don't assert becasue the catalog changes.

--- catalog/catalog_bench_test.go
+++ catalog/catalog_bench_test.go
@@ -28,32 +28,39 @@ func benchmarkGetBrandsWithOptions(opts *modelsCatalog.CatalogQueryOptions, b *t
 }
 
 // 56487095 ns/op
+// 13938 results
 func Benchmark_GetEndpoint_NoFilter(b *testing.B) { benchmarkGetEndpointsWithOptions(nil, b) }
 
 // 56367765 ns/op
+// 1 result
 func Benchmark_GetEndpoint_SingleEndpoint(b *testing.B) {
 	benchmarkGetEndpointsWithOptions(&modelsCatalog.CatalogQueryOptions{Id: "9d0fa28a-0c5b-4065-9ee6-284ec9577a57"}, b)
 }
 
 // 55848260 ns/op
+// 36 results
 func Benchmark_GetEndpoint_Environment(b *testing.B) {
 	benchmarkGetEndpointsWithOptions(&modelsCatalog.CatalogQueryOptions{LighthouseEnvType: pkg.FastenLighthouseEnvSandbox}, b)
 }
 
 // 182056250 ns/op
+// 40167
 func Benchmark_GetPortals_NoFilter(b *testing.B) { benchmarkGetPortalsWithOptions(nil, b) }
 
-// 181978688 ns/op
+// 278172469 ns/op
+// 1 result
 func Benchmark_GetPortals_SingleEndpoint(b *testing.B) {
 	benchmarkGetPortalsWithOptions(&modelsCatalog.CatalogQueryOptions{Id: "59673c08-e4b5-44d5-b5ab-532e69e8f7e7"}, b)
 }
 
-// 300082958 ns/op - double time
+// 539327104 ns/op - double time
+// 37 results
 func Benchmark_GetPortals_Environment(b *testing.B) {
 	benchmarkGetPortalsWithOptions(&modelsCatalog.CatalogQueryOptions{LighthouseEnvType: pkg.FastenLighthouseEnvSandbox}, b)
 }
 
-// 194024736 ns/op
+// 316843417 ns/op
+// 37 results
 func Benchmark_GetPortals_Environment_WithEndpointCache(b *testing.B) {
 	cache, _ := GetEndpoints(&modelsCatalog.CatalogQueryOptions{LighthouseEnvType: pkg.FastenLighthouseEnvSandbox})
 	benchmarkGetPortalsWithOptions(&modelsCatalog.CatalogQueryOptions{
@@ -62,21 +69,24 @@ func Benchmark_GetPortals_Environment_WithEndpointCache(b *testing.B) {
 	}, b)
 }
 
-// 203611925 ns/op
-// 2722 ns/op
+// 504037062 ns/op
+// 33329 results
 func Benchmark_GetBrands_NoFilter(b *testing.B) { benchmarkGetBrandsWithOptions(nil, b) }
 
-// 205480108 ns/op
+// 519999334 ns/op
+// 0 results
 func Benchmark_GetBrands_SingleEndpoint(b *testing.B) {
 	benchmarkGetBrandsWithOptions(&modelsCatalog.CatalogQueryOptions{Id: "59673c08-e4b5-44d5-b5ab-532e69e8f7e7"}, b)
 }
 
-// 3094676666 ns/op - 12 times slower
+// 8634036542 ns/op - 12 times slower
+// 33 results
 func Benchmark_GetBrands_Environment_Sandbox(b *testing.B) {
 	benchmarkGetBrandsWithOptions(&modelsCatalog.CatalogQueryOptions{LighthouseEnvType: pkg.FastenLighthouseEnvSandbox}, b)
 }
 
-// 267615969 ns/op
+// 1007805916 ns/op
+// 33 results
 func Benchmark_GetBrands_Environment_Sandbox_WithEndpointCache(b *testing.B) {
 	cacheEndpoints, _ := GetEndpoints(&modelsCatalog.CatalogQueryOptions{LighthouseEnvType: pkg.FastenLighthouseEnvSandbox})
 	cachePortals, _ := GetPortals(&modelsCatalog.CatalogQueryOptions{LighthouseEnvType: pkg.FastenLighthouseEnvSandbox, CachedEndpointsLookup: &cacheEndpoints})
@@ -91,7 +101,8 @@ func Benchmark_GetBrands_Environment_Sandbox_WithEndpointCache(b *testing.B) {
 //	benchmarkGetBrandsWithOptions(&modelsCatalog.CatalogQueryOptions{LighthouseEnvType: pkg.FastenLighthouseEnvProduction}, b)
 //}
 
-// 1413393208 ns/op
+// 4728215542 ns/op
+// 33307 results
 func Benchmark_GetBrands_Environment_Prod_WithEndpointCache(b *testing.B) {
 	cacheEndpoints, _ := GetEndpoints(&modelsCatalog.CatalogQueryOptions{LighthouseEnvType: pkg.FastenLighthouseEnvProduction})
 	cachePortals, _ := GetPortals(&modelsCatalog.CatalogQueryOptions{LighthouseEnvType: pkg.FastenLighthouseEnvProduction, CachedEndpointsLookup: &cacheEndpoints})
